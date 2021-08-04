f70c371fc1eb1a3c0a4be4af24963c544d684ea4
mitjasha <romantus_@mail.ru>
2021-08-04 15:58:12 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number){
+    let out = 0;
+    while(number !=0){
+        out *= 10;
+        out += number % 10;
+        number = (number - number % 10) / 10;
+    }
+    return Math.abs(out);
 }
+
+// module.exports = function reverse (n) {
+//     let n_str = Math.abs(n).toString()
+//     let out = ''
+//     for (let i = n_str.length - 1; i >= 0; i--){
+//         out += n_str[i]
+//     }
+//     return Number(out);
+// }
